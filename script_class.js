2904c143bf496daa248c27e90d0e08524de55cbf
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-07 15:08:09 +0100

    modify script class

--- script_class.js
+++ script_class.js
@@ -1,7 +1,25 @@
 class UserList {
-    constructor(container) {
+    constructor(container, numberOfUsers) {
         this.container = container
+        this.numberOfUsers = numberOfUsers
         this.users = []
+
+        this.init()
+    }
+
+    init() {
+        this.render()
+        this.fetchUsers()
+    }
+
+    fetchUsers() {
+        fetch('https://randomuser.me/api/?results=' + this.numberOfUsers)
+            .then(response => response.json())
+            .then(this.setUsers.bind(this))
+    }
+
+    setUsers(data) {
+        this.users = data.results
         this.render()
     }
 
@@ -9,11 +27,33 @@ class UserList {
         this.container.innerHTML = ''
 
         const ul = document.createElement('ul')
-        this.users.forEach(user => {
-            li.innerText = `$(name.first)$(name.last)`
+
+        this.users.forEach((user, userIndex) => {
+            const li = document.createElement('li')
+            const button = document.createElement('button')
+
+            li.innerText = `${user.name.first} ${user.name.last}`
+            button.innerText = 'X'
+
+            li.addEventListener('click', (e) => this.onUserClickHandler(user))
+            button.addEventListener('click', (e) => this.onUserDeleteClickHandler(e, userIndex))
+
+            li.appendChild(button)
+            ul.appendChild(li)
         })
+
         this.container.appendChild(ul)
+    }
+
+    onUserClickHandler(user) {
+        alert(user.email)
+    }
+
+    onUserDeleteClickHandler(e, userIndex) {
+        e.stopPropagation()
+        this.users = this.users.slice(0, userIndex).concat(this.users.slice(userIndex + 1))
+        this.render()
     }
 }
 
-const list = new UserList(document.body)
+const list = new UserList(document.body, 4)
